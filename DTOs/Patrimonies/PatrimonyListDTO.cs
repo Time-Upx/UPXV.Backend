48e4f43864607c2cdbcf216860766eafebd480da
ThomasJanoski <thomassilveira2020@gmail.com>
2025-11-13 22:22:57 -0300

    Added extra fields to patrimonies' and qr-code's list dtos

--- DTOs/Patrimonies/PatrimonyListDTO.cs
+++ DTOs/Patrimonies/PatrimonyListDTO.cs
@@ -10,11 +10,13 @@ public record PatrimonyListDTO
    public required string Name { get; set; }
    public IEnumerable<TagListDTO> Tags { get; set; } = [];
    public required StatusListDTO Status { get; set; }
-   public static PatrimonyListDTO Of (Patrimony patrimony) => new PatrimonyListDTO
+   public DateTime RegisteredIn { get; set; }
+   public static PatrimonyListDTO Of(Patrimony patrimony) => new PatrimonyListDTO
    {
       Id = patrimony.Id,
       Name = patrimony.Name,
       Tags = patrimony.Tags.Select(TagListDTO.Of),
       Status = StatusListDTO.Of(patrimony.Status!),
+      RegisteredIn = patrimony.RegisteredIn,
    };
 }
--- DTOs/QRCodes/QRCodeListDTO.cs
+++ DTOs/QRCodes/QRCodeListDTO.cs
@@ -8,15 +8,20 @@ public record QRCodeListDTO
    public required string Id { get; set; }
    public IntentListDTO? Intent { get; set; }
    public string? Name { get; set; }
-   public bool HasExpired { get; set; }
-   public bool HasReachedUsageLimit { get; set; }
+   public DateTime CreatedAt { get; set; }
+   public DateTime? Expiration { get; set; }
+   public int? UsageLimit { get; set; }
+   public int TimesUsed { get; set; }
 
-   public static QRCodeListDTO Of (QRCode qrcode) => new()
+
+   public static QRCodeListDTO Of(QRCode qrcode) => new()
    {
       Id = qrcode.Id,
-      Intent = IntentListDTO.Of (qrcode.Intent!),
+      Intent = IntentListDTO.Of(qrcode.Intent!),
       Name = qrcode.Name,
-      HasExpired = qrcode.HasExpired,
-      HasReachedUsageLimit = qrcode.HasReachedUsageLimit,
+      CreatedAt = qrcode.CreatedAt,
+      Expiration = qrcode.Expiration,
+      UsageLimit = qrcode.UsageLimit,
+      TimesUsed = qrcode.TimesUsed,
    };
 }
